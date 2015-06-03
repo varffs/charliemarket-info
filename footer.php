e8363c4e5550fb5131565c3c5291a75046d7a6fb
Patrick Best <patrickbest@patrickbest.com>
2015-06-03 18:34:50 -0500

    init basic theme

---
+++ footer.php
@@ -0,0 +1,10 @@
+    <footer id="footer" class="u-align-center">
+      <?php wp_nav_menu('theme_location=footer_menu'); ?>
+    </footer>
+
+  </section>
+
+  <?php get_template_part('partials/scripts'); ?>
+
+  </body>
+</html>
---
+++ index.php
@@ -0,0 +1,53 @@
+<?php
+get_header();
+?>
+
+<!-- main content -->
+
+<main id="main-content">
+
+  <!-- main posts loop -->
+  <section id="posts">
+
+<?php
+if( have_posts() ) {
+  while( have_posts() ) {
+    the_post();
+    $meta = get_post_meta($post->ID);
+?>
+
+    <article <?php post_class(); ?> id="post-<?php the_ID(); ?>" style="<?php
+      if (!empty($meta['_igv_left'][0])) {
+        echo 'padding-left:' . $meta['_igv_left'][0] . '%;';
+      }
+      if (!empty($meta['_igv_right'][0])) {
+        echo 'padding-right:' . $meta['_igv_right'][0] . '%;';
+      }
+?>">
+
+      <a href="<?php the_permalink() ?>"><?php the_title(); ?></a>
+
+      <?php the_content(); ?>
+
+    </article>
+
+<?php
+  }
+} else {
+?>
+    <article class="u-alert"><?php _e('Sorry, no posts matched your criteria :{'); ?></article>
+<?php
+} ?>
+
+  <!-- end posts -->
+  </section>
+
+  <?php get_template_part('partials/pagination'); ?>
+
+<!-- end main-content -->
+
+</main>
+
+<?php
+get_footer();
+?>
